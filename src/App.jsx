1970a01731e12e34b7a4286ac0e58c89768e6c3f
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-10 23:13:36 +0530

    App Structure Completed With Main Styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,22 @@ import styles from "./style";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>Hello World</div>
+    {/* Navbar-Section */}
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>Navbar</div>
+    </div>
+
+    {/* Hero-Section */}
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>Hero</div>
+    </div>
+
+    {/* Other-Sections */}
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats Business Billing CardDeal Testimonials Clients CTA Footer
+      </div>
+    </div>
   </div>
 );
 
