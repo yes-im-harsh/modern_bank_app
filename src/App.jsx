c7a03f0f7b931b14d24ce38f537ad10c83f5a3df
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-10 22:57:00 +0530

    Added constants & style.js files for convenience :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import styles from "./style";
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>Hello World</div>
+  </div>
+);
 
-export default App
+export default App;
