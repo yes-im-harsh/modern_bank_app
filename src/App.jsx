c6eb64a88c464abe27e49afe5ec0e8eae827d4f0
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-10 23:31:18 +0530

    Components Created & Imported(New Way*) In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
 import React from "react";
 import styles from "./style";
 
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
+
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     {/* Navbar-Section */}
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
     </div>
 
     {/* Hero-Section */}
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero</div>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
     </div>
 
     {/* Other-Sections */}
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats Business Billing CardDeal Testimonials Clients CTA Footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
