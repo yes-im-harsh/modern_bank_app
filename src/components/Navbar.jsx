283599010e67617e7572fac7f7fea05cf4740bd0
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-10 23:51:07 +0530

    Navbar Part-1 Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-full py-4 flex justify-between items-center navbar">
+      <img src={logo} alt="hoobank" className="w-[120px] h-[32px]" />
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={` font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
