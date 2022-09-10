ca23bcc6344306aab8936b46a91f202ba2efde7f
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-11 01:35:02 +0530

    Responsive Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import { useState } from "react";
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full py-4 flex justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[120px] h-[32px]" />
@@ -18,6 +20,34 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="object-contain w-[28px] h-[28px]"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={` ${
+            toggle ? "flex" : "hidden"
+          } bg-black-gradient absolute top-16 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar p-6`}
+        >
+          <ul className="list-none flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mb-0" : "mb-3"
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
