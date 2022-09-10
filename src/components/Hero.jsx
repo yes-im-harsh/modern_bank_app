45ad8d7c0bdcaac9a02da6fb6b491d85f12d0b0f
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-11 02:33:03 +0530

    Discount & Header Section Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { discount, robot } from "../assets";
+import styles from "../style";
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+const Hero = () => (
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div
+      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+    >
+      {/* Discount Part */}
+
+      <div className="flex flex-row items-center py-[4px] px-3 bg-discount-gradient rounded-[8px] mb-1">
+        <img src={discount} alt="discount" className=" w-[28px] h-[28px]" />
+        <p className={`ml-1 ${styles.paragraph}`}>
+          <span className="text-white">20%</span> Discount For{" "}
+          <span className="text-white">1 Month</span> Account
+        </p>
+      </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        {/* Text Part */}
+        <h1 className="flex-1 font-poppins text-white ss:text-[62px] text-[42px] ss:leading-[90px] leading-[65px]">
+          The Next <br className="sm:block hidden" />{" "}
+          <span className="text-gradient">Generation </span>
+          <br className="sm:block hidden" /> Payment Method
+        </h1>
+      </div>
+    </div>
+  </section>
+);
 
 export default Hero;
