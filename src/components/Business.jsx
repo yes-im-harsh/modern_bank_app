3ccbece8d6279cb516bfa8ca480de822d5076188
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-12 21:51:43 +0530

    Left Side of Business Component  done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we'll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit card on the market
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default Business;
