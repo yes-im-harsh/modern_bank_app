8c36f83930fc64ccc951c9781ab0eed1ec433033
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-16 21:59:29 +0530

    Footer Left Side Implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import styles from "../style";
+import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <div className={`w-full mb-8 ${styles.flexStart} flex-col md:flex-row`}>
+      <div className="flex flex-1 flex-col justify-start mr-10">
+        <img
+          src={logo}
+          alt="hoobank"
+          className="w-[266px] h-[72px] object-contain"
+        />
+        <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+          A new way to make the payments easy, reliable and secure.
+        </p>
+      </div>
+    </div>
+  </section>
+);
 
 export default Footer;
