b930e8c67baafc66566bbcda9990ac4e6280e7bf
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-16 16:36:31 +0530

    Testimonials Part 1 Completed

---
+++ src/components/FeedbackCard.jsx
@@ -0,0 +1,5 @@
+import { quotes } from "../assets";
+
+const FeedbackCard = ({ content, name, title, img }) => console.log(content);
+
+export default FeedbackCard;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
-const Testimonials = () => {
-  return <div>Testimonials</div>;
-};
+const Testimonials = () => (
+  <section
+    id="clients"
+    className={`${styles.flexCenter} ${styles.paddingY} flex-col relative`}
+  >
+    <div />
+
+    {/* Headings */}
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 z-[1]">
+      <h2 className={styles.heading2}>
+        What people are <br className="sm:block hidden" /> saying about us
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} max-w-[420px] text-left`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+    </div>
+
+    <div className="flex flex-wrap relative w-full sm:justify-start justify-center feedback-container z-[1]">
+      {feedback.map((card) => (
+        <FeedbackCard id={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+);
 
 export default Testimonials;
